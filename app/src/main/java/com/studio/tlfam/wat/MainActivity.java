f81b4c3be3c2386e4beb11a1b403dec8112177ee
Tanguy Le Floch <tanguy@Tanguy-G53SX.local.dev>
2017-01-09 21:25:43 +0100

    Button playing

--- app/src/main/java/com/studio/tlfam/wat/MainActivity.java
+++ app/src/main/java/com/studio/tlfam/wat/MainActivity.java
@@ -1,19 +1,45 @@
 package com.studio.tlfam.wat;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.widget.TextViewCompat;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+    Button monbouton = null;
+    TextView myText = null;
+
+
+    private View.OnClickListener clickListenerButtons = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(monbouton.getText() == "Yolo")
+                monbouton.setText("Lala");
+            else
+                monbouton.setText("Yolo");
+        }
+    };
+
+    private View.OnTouchListener touchListenerButtons = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            monbouton.setTextSize(Math.abs(event.getX() - monbouton.getWidth() / 2) +
+                                  Math.abs(event.getY() - monbouton.getHeight() / 2));
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.exercice_layout);
 
-        TextView text = new TextView(this);
-        text.setText("Bonjour, voila la première activité de WAT !");
-        setContentView(text);
+        monbouton = (Button)findViewById(R.id.bouton);
+        monbouton.setOnTouchListener(touchListenerButtons);
     }
 }
---
+++ app/src/main/java/com/studio/tlfam/wat/SecondaryActivity.java
@@ -0,0 +1,20 @@
+package com.studio.tlfam.wat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class SecondaryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView view = new TextView(this);
+
+        view.setText("Activity 2");
+
+        setContentView(view);
+    }
+}
